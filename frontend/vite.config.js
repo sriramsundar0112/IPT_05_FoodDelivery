6dd293267aa8055295cddee7537f97cd084be220
Sriram0112 <devops2t25@gmail.com>
2026-07-06 12:01:39 -0700

    Reverting all the incorrect commits made by the Team

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,13 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
- 
-export default defineConfig({
 
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-  server: {
-
-  host: '0.0.0.0',
-
-  port: 5173,
-
-  allowedHosts: [
-
-      'food-delivery-alb-863219639.us-east-2.elb.amazonaws.com'
-
-    ]
-
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+    },
   }
-
 })
- 
