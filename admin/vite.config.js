15e5bdef47422be4315a105d4ead28196df440c8
suryasivaraj18 <suryasivaraj18@gmail.com>
2026-07-06 18:00:42 +0530

    adding the root path

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,29 +1,20 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
-
   plugins: [react()],
- 
+
+  base: '/admin/',
+
   server: {
-
     host: '0.0.0.0',
-
     port: 5174,
-
     allowedHosts: true
-
   },
- 
+
   preview: {
-
     host: '0.0.0.0',
-
     port: 5174,
-
     allowedHosts: true
-
   }
-
 })
