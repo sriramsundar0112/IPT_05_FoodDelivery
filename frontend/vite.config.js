20f4b29867300f2c0b731029e909586b03463c20
suryasivaraj18 <suryasivaraj18@gmail.com>
2026-07-07 12:09:44 +0530

    Configure Vite server / allowing host
    This allows Vite to listen on all network interfaces inside Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,21 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
+
     server: {
+      host: '0.0.0.0',
       port: Number(env.VITE_PORT) || 5173,
+      allowedHosts: true
     },
+
+    preview: {
+      host: '0.0.0.0',
+      port: Number(env.VITE_PORT) || 5173,
+      allowedHosts: true
+    }
   }
 })
